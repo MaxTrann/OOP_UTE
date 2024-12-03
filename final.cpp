cf7aab1490d4a90cebb65183148e24ebade1edfb
MaxTrann <tranlequocdai2005@gmail.com>
2024-12-03 12:26:23 +0700

    Tách cập nhật vay mượn

--- final.cpp
+++ final.cpp
@@ -16,14 +16,16 @@ void showMenu() {
     cout << "8. Xuat thong tin nguoi dung" << endl;
     cout << "9. Xuat danh sach tai khoan" << endl;
     cout << "10. Xem thong tin so du" << endl;
-    cout << "11. Xem so du cua tung tai khoan" << endl; // Tùy chọn mới
-    cout << "12. Thoat" << endl;
+    cout << "11. Xem so du cua tung tai khoan" << endl;
+    cout << "12. Cap nhat giao dich" << endl;
+    cout << "13. Cap nhat khoan vay" << endl;
+    cout << "14. Cap nhat khoan muon" << endl;
+    cout << "15. Xoa giao dich" << endl;
+    cout << "16. Xoa khoan vay/muon" << endl;
+    cout << "17. Thoat" << endl;
     cout << "=============================================" << endl;
     cout << "Chon mot tuy chon: ";
 }
-
-
-
 
 int main() {
     User user;
@@ -36,31 +38,26 @@ int main() {
         cin.ignore();
 
         switch (choice) {
-            case 1: {
+            case 1:
                 cout << "=== Them nguoi dung moi ===" << endl;
                 user.input();
                 cout << "Nguoi dung da duoc them thanh cong!" << endl;
                 break;
-            }
-            case 2: {
+            case 2:
                 cout << "=== Them tai khoan ===" << endl;
                 user.addAcount();
                 break;
-            }
-            case 3: {
+            case 3:
                 cout << "=== Thuc hien giao dich ===" << endl;
                 user.addTransaction();
                 break;
-            }
-            case 4: {
+            case 4:
                 cout << "=== Thuc hien vay/muon ===" << endl;
                 cout << "1. Them khoan vay" << endl;
                 cout << "2. Them khoan muon" << endl;
                 int subChoice;
                 cout << "Nhap lua chon: ";
                 cin >> subChoice;
-
-                cin.ignore();
 
                 if (subChoice == 1) {
                     user.addLoan();
@@ -70,46 +67,58 @@ int main() {
                     cout << "Lua chon khong hop le!" << endl;
                 }
                 break;
-            }
-            case 5: {
+            case 5:
                 cout << "=== Hien thi lich su giao dich ===" << endl;
                 user.transactionHistory();
                 break;
-            }
-            case 6: {
+            case 6:
                 cout << "=== Hien thi bao cao tai khoan ===" << endl;
                 user.reportAccount();
                 break;
-            }
-            case 7: {
+            case 7:
                 cout << "=== Hien thi bao cao khoan vay/muon ===" << endl;
                 user.reportLoan_Borrow();
                 break;
-            }
-            case 8: {
+            case 8:
                 cout << "=== Xuat thong tin nguoi dung ===" << endl;
                 user.display();
                 break;
-            }
-            case 9: {
+            case 9:
                 cout << "=== Xuat danh sach tai khoan ===" << endl;
                 user.showAccount();
                 break;
-            }
-            case 10: {
+            case 10:
                 cout << "=== Xem thong tin so du ===" << endl;
                 user.displayBalanceInfo();
                 break;
-            }
-            case 11: {
+            case 11:
                 cout << "=== Xem so du cua tung tai khoan ===" << endl;
                 user.displayAllAccountBalances();
                 break;
-            }
-            case 12: {
+            case 12:
+                cout << "=== Cap nhat giao dich ===" << endl;
+                user.updateTransaction();
+                break;
+            case 13:
+                cout << "=== Cap nhat khoan vay ===" << endl;
+                user.updateLoan();
+                break;
+            case 14:
+                cout << "=== Cap nhat khoan muon ===" << endl;
+                user.updateBorow();
+                break;
+            case 15:
+                cout << "=== Xoa giao dich ===" << endl;
+                user.deleteTransaction();
+                break;
+            case 16:
+                cout << "=== Xoa khoan vay/muon ===" << endl;
+                user.deleteLoan();
+                user.deleteBorrow();
+                break;
+            case 17:
                 cout << "Thoat chuong trinh. Tam biet!" << endl;
                 return 0;
-            }
             default:
                 cout << "Lua chon khong hop le, vui long chon lai." << endl;
         }
