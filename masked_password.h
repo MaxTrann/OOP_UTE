2c26f938698af02a0507a2a877f828fedabf5793
dien2005 <143526661+dien2005@users.noreply.github.com>
2024-12-03 10:51:51 +0700

    Create masked_password.h

---
+++ masked_password.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+string getMaskedPassword() {
+    string password;
+    char ch;
+
+    cout << "Enter password: ";
+    while ((ch = _getch()) != '\r') {
+        if (ch == '\b') {
+            if (!password.empty()) {
+                password.pop_back();
+                cout << "\b \b";
+            }
+        } else {
+            password += ch;
+            cout << '*';
+        }
+    }
+    cout << endl;
+    return password;
+}
+// cach su dung: include thu vien vao
+// string password = getMaskedPassword()
